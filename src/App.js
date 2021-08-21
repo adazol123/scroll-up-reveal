7f3cf75cb0b6c364d06455b9d2f96a052c16b0d0
Daniel Lozada <dpadayao40@gmail.com>
2021-08-20 20:59:17 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { motion } from 'framer-motion'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [toggleActions, setToggleActions] = useState(false)
+  const [ lastYPos, setLastYPos] = useState(0)
+
+  useEffect(() => {
+
+    function handleScroll(){
+      const yPos = window.scrollY
+      const isScrollingUp = yPos < lastYPos
+
+      setToggleActions(isScrollingUp)
+      setLastYPos(yPos)
+    }
+
+    window.addEventListener('scroll', handleScroll, false)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll, false)
+    }
+  }, [lastYPos])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="section">
+      <div className="cover dark">
+          <h2>Hello</h2>
+      </div>
+      <div className="cover light">
+          <h1>Hello</h1>
+      </div>
+
+
+      {toggleActions && <motion.div className="action"
+        initial={{ opacity: 0}}
+        animate={{ opacity: 1}}
+        transition={{ opacity: { duration: 0.4}}}
+      >
+        <button onClick={() => console.log('clicked!')}>Home</button>
+        <button onClick={() => console.log('clicked!')}>profile</button>
+        <button onClick={() => console.log('clicked!')}>More</button>
+      </motion.div>}
     </div>
   );
 }
